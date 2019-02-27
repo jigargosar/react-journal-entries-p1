b763742032ced1cda4fd03b5ffb680187ccc6e4c
Jigar Gosar <jigar.gosar@gmail.com>
2019-02-27 17:43:54 +0530

    appModel

---
+++ src/comp/NewEntryItem.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import Hotkeys from 'react-hot-keys'
+import { observer } from 'mobx-react-lite'
+
+function NewEntryItem({ content, effects }) {
+  return (
+    <div className="pv2 code">
+      <Hotkeys
+        keyName="control+enter,command+enter"
+        onKeyDown={kn => {
+          switch (kn) {
+            case 'control+enter':
+            case 'command+enter':
+              return effects.saveNewEntry()
+            default:
+              break
+          }
+        }}
+      >
+        <textarea
+          id="new-entry-input"
+          autoFocus
+          className="w-100 pa2 ma0"
+          style={{ minHeight: 200 }}
+          value={content}
+          onChange={effects.onNewEntryContentChange}
+        />
+      </Hotkeys>
+    </div>
+  )
+}
+
+export default observer(NewEntryItem)
---
+++ src/useAppModel.js
@@ -0,0 +1,160 @@
+import { useEffect, useMemo, useState } from 'react'
+import * as R from 'ramda'
+import faker from 'faker'
+import { _, it } from 'param.macro'
+import PouchDb from 'pouchdb-browser'
+import { observable } from 'mobx'
+import { getCached, setCache } from './cache-helpers'
+import validate from 'aproba'
+import * as nanoid from 'nanoid'
+
+const db = new PouchDb('journal-entries')
+
+function useActions(setModel) {
+  function wrapWithSetModel(updaters) {
+    return R.mapObjIndexed(fn =>
+      R.compose(
+        setModel,
+        fn,
+      ),
+    )(updaters)
+  }
+
+  return useMemo(
+    () =>
+      wrapWithSetModel({
+        addNew: () =>
+          R.mergeLeft({ newEntryContent: '', showNewEntry: true }),
+        closeNew: () =>
+          R.mergeLeft({ newEntryContent: '', showNewEntry: false }),
+        setNewEntryContent: content =>
+          R.mergeLeft({ newEntryContent: content }),
+        setLastErrMsg(err) {
+          console.error('setLastErrMsg', err)
+          return R.assoc('lastErrMsg')(err.message)
+        },
+        handleEntryDbChange(change) {
+          return model => {
+            if (change.deleted) {
+              return R.dissocPath(['entryById', change.id])(model)
+            } else {
+              const doc = change.doc
+              return R.assocPath(['entryById', doc._id])(doc)(model)
+            }
+          }
+        },
+      }),
+    [],
+  )
+}
+
+function createNewEntry({ content }) {
+  validate('O', arguments)
+  validate('S', [content])
+
+  return {
+    _id: `e_${nanoid()}`,
+    content: content,
+    createdAt: Date.now(),
+    modifiedAt: Date.now(),
+  }
+}
+
+function useEffects(actions, model) {
+  return useMemo(() => {
+    const otherwiseHandlePouchDbError = R.otherwise(actions.setLastErrMsg)
+
+    return {
+      onFakeAddClicked: () => {
+        const newEntry = createNewEntry({
+          content: faker.lorem.paragraph(),
+        })
+        R.pipe(
+          it.put(newEntry),
+          otherwiseHandlePouchDbError,
+        )(db)
+      },
+      onAddNewClicked: () => actions.addNew(),
+      onNewEntryContentChange: e =>
+        actions.setNewEntryContent(e.target.value),
+      saveNewEntry: content => {
+        const newEntry = createNewEntry({
+          content: model.newEntryContent,
+        })
+        R.pipe(
+          it.put(newEntry),
+          R.then(() => actions.closeNew()),
+          otherwiseHandlePouchDbError,
+        )(db)
+      },
+      onDeleteAllClicked: () =>
+        R.pipe(
+          it.allDocs({ include_docs: true }),
+          R.then(
+            R.pipe(
+              R.prop('rows'),
+              R.pluck('doc'),
+              R.map(R.mergeLeft({ _deleted: true })),
+              db.bulkDocs(_),
+            ),
+          ),
+          R.then(console.log('bulkDocsResponse', _)),
+          otherwiseHandlePouchDbError,
+        )(db),
+    }
+  }, [])
+}
+
+export function useAppModel() {
+  const [model] = useState(() =>
+    R.compose(
+      observable.object,
+      R.mergeDeepRight({
+        entryById: {},
+        lastErrMsg: null,
+        showNewEntry: false,
+        newEntryContent: '',
+      }),
+      R.defaultTo({}),
+      getCached,
+    )('appModel'),
+  )
+
+  if (process.env.NODE_ENV !== 'production') {
+    window.model = model
+  }
+
+  function setModel(fn) {
+    const newModel = fn(model)
+    R.forEachObjIndexed((v, k) => {
+      model[k] = v
+    })(newModel)
+  }
+
+  const actions = useActions(setModel)
+
+  useEffect(() => setCache('appModel', model), [model])
+
+  useEffect(() => {
+    const changes = db
+      .changes({ include_docs: true, live: true })
+      .on('change', actions.handleEntryDbChange)
+      .on('error', actions.setLastErrMsg)
+    return () => changes.cancel()
+  }, [])
+
+  const effects = useEffects(actions, model)
+
+  return [model, effects]
+}
+
+export function getDisplayEntries(model) {
+  return R.compose(
+    R.sortWith([R.descend(R.prop('createdAt'))]),
+    R.values,
+  )(model.entryById)
+}
+
+export function getAllEntries(model) {
+  return R.values(model.entryById)
+}
