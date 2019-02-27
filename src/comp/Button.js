645f1855f84cc540d83a29da318dd84936b2ae3a
Jigar Gosar <jigar.gosar@gmail.com>
2019-02-27 17:44:39 +0530

    appModel

--- src/comp/Button.js
+++ src/comp/Button.js
@@ -2,13 +2,7 @@ import React from 'react'
 import { observer } from 'mobx-react-lite'
 
 function Button(props) {
-  return (
-    <button
-      role="button"
-      className="br1 ba b--black-20 ttu mh1 f6"
-      {...props}
-    />
-  )
+  return <button className="br1 ba b--black-20 ttu mh1 f6" {...props} />
 }
 
 export default observer(Button)
