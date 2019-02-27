675db023de3b74b77a536c967a7ab5adb1a2982c
Jigar Gosar <jigar.gosar@gmail.com>
2019-02-27 17:39:01 +0530

    appModel

--- src/App.js
+++ src/App.js
@@ -1,14 +1,116 @@
-import React, { useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import { observer } from 'mobx-react-lite'
 import { observable } from 'mobx'
 import * as R from 'ramda'
-import { getCached } from './cache-helpers'
+import { getCached, setCache } from './cache-helpers'
 import EntryItem from './comp/EntryItem'
 import Hotkeys from 'react-hot-keys'
 import Button from './comp/Button'
+import PouchDb from 'pouchdb-browser'
+import { _, it } from 'param.macro'
+import faker from 'faker'
+import validate from 'aproba'
+import * as nanoid from 'nanoid'
+
+const db = new PouchDb('journal-entries')
+
+function useActions(setModel) {
+  function wrapWithSetModel(updaters) {
+    return R.mapObjIndexed(fn =>
+      R.compose(
+        setModel,
+        fn,
+      ),
+    )(updaters)
+  }
+
+  return useMemo(
+    () =>
+      wrapWithSetModel({
+        addNew: () =>
+          R.mergeLeft({ newEntryContent: '', showNewEntry: true }),
+        closeNew: () =>
+          R.mergeLeft({ newEntryContent: '', showNewEntry: false }),
+        setNewEntryContent: content =>
+          R.mergeLeft({ newEntryContent: content }),
+        setLastErrMsg(err) {
+          console.error('setLastErrMsg', err)
+          return R.assoc('lastErrMsg')(err.message)
+        },
+        handleEntryDbChange(change) {
+          return model => {
+            if (change.deleted) {
+              return R.dissocPath(['entryById', change.id])(model)
+            } else {
+              const doc = change.doc
+              return R.assocPath(['entryById', doc._id])(doc)(model)
+            }
+          }
+        },
+      }),
+    [],
+  )
+}
+
+function createNewEntry({ content }) {
+  validate('O', arguments)
+  validate('S', [content])
+
+  return {
+    _id: nanoid(),
+    content: content,
+    createdAt: Date.now(),
+    modifiedAt: Date.now(),
+  }
+}
+
+function useEffects(actions) {
+  return useMemo(() => {
+    const otherwiseHandlePouchDbError = R.otherwise(actions.setLastErrMsg)
+
+    return {
+      onFakeAddClicked: () => {
+        const newEntry = createNewEntry({
+          content: faker.lorem.paragraph(),
+        })
+        R.pipe(
+          it.put(newEntry),
+          otherwiseHandlePouchDbError,
+        )(db)
+      },
+      onAddNewClicked: () => actions.addNew(),
+      onNewEntryContentChange: e =>
+        actions.setNewEntryContent(e.target.value),
+      saveNewEntry: content => {
+        const newEntry = createNewEntry({
+          content: content,
+        })
+        R.pipe(
+          it.put(newEntry),
+          R.then(() => actions.closeNew()),
+          otherwiseHandlePouchDbError,
+        )(db)
+      },
+      onDeleteAllClicked: () =>
+        R.pipe(
+          it.allDocs({ include_docs: true }),
+          R.then(
+            R.pipe(
+              R.prop('rows'),
+              R.pluck('doc'),
+              R.map(R.mergeLeft({ _deleted: true })),
+              db.bulkDocs(_),
+            ),
+          ),
+          R.then(console.log('bulkDocsResponse', _)),
+          otherwiseHandlePouchDbError,
+        )(db),
+    }
+  }, [])
+}
 
 function useAppModel() {
-  return useState(() =>
+  const [model] = useState(() =>
     R.compose(
       observable.object,
       R.mergeDeepRight({
@@ -21,6 +123,33 @@ function useAppModel() {
       getCached,
     )('appModel'),
   )
+
+  if (process.env.NODE_ENV !== 'production') {
+    window.model = model
+  }
+
+  function setModel(fn) {
+    const newModel = fn(model)
+    R.forEachObjIndexed((v, k) => {
+      model[k] = v
+    })(newModel)
+  }
+
+  const actions = useActions(setModel)
+
+  useEffect(() => setCache('appModel', model), [model])
+
+  useEffect(() => {
+    const changes = db
+      .changes({ include_docs: true, live: true })
+      .on('change', actions.handleEntryDbChange)
+      .on('error', actions.setLastErrMsg)
+    return () => changes.cancel()
+  }, [])
+
+  const effects = useEffects(actions)
+
+  return [model, effects]
 }
 
 export function getDisplayEntries(model) {
